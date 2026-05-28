6fd846367c99d0196bae66efe8378ddb81139a83
Nadim khan <khanm99098@gmail.com>
2026-05-28 12:11:35 +0530

    solved importing issue in directoryModel.js file

--- helper/updateDirectorySize.js
+++ helper/updateDirectorySize.js
@@ -1,20 +1,18 @@
-import Directory from "../models/DirectoryModel.js";
+import Directory from "../models/directoryModel.js";
 
+export const updateDirectorySize = async (parentId, sizeToAdd) => {
+  const parentIdCollection = [];
+  while (parentId) {
+    parentIdCollection.push(parentId);
+    const currentDirectory = await Directory.findById(parentId).lean();
+    if (!currentDirectory.parentDirId) break;
+    parentId = currentDirectory.parentDirId;
+  }
 
-export const updateDirectorySize=async (parentId,sizeToAdd)=>{
-   const parentIdCollection=[];
-        while(parentId){
-           parentIdCollection.push(parentId);
-           const currentDirectory=await Directory.findById(parentId).lean();
-           if(!currentDirectory.parentDirId) break;
-           parentId=currentDirectory.parentDirId;
-        }
-
-        if(parentIdCollection.length > 0){
-          await Directory.updateMany(
-         { _id: { $in: parentIdCollection } },
-        { $inc: { size: sizeToAdd } }
-        );
-        }
-      
-}
+  if (parentIdCollection.length > 0) {
+    await Directory.updateMany(
+      { _id: { $in: parentIdCollection } },
+      { $inc: { size: sizeToAdd } },
+    );
+  }
+};
