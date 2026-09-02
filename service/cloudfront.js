c1e3dc523127d551444c3324fc73df69da38950b
wasicodess <mwkhanb23@gmail.com>
2026-09-02 21:03:05 +0530

    Updated project

--- service/cloudfront.js
+++ service/cloudfront.js
@@ -2,7 +2,7 @@ import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 
 const distributionKey=`https://d1x4vx6q84mqky.cloudfront.net`;
 const privateKey=process.env.CLOUDFRONT_PRIVATE_KEY;
-const keyPairId="K2I3KC7U2XPMRJ";
+const  keyPairId="K2I3KC7U2XPMRJ";
 
 export const createCloudFrontSignedUrl=(key,queryParams={})=>{
   const url=new URL(`${distributionKey}/${key}`);
